149c4c2ebfaa198f9b58dc72dacc4cd6ee3377cb
bayuyuhartono-katadata <bayu.yuhartono@katadata.co.id>
2021-09-11 01:44:02 +0700

    slicing

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -37,7 +37,7 @@
 
     .terkini-wrap {
         overflow: hidden;
-        border-bottom: 3px solid #CCCCCC;
+        border-bottom: 2px solid #CCCCCC;
     }
 
     .terkini-button {
@@ -45,69 +45,125 @@
         display: block;
         color: black;
         text-align: center;
-        padding: 0px;
+        padding: 3px;
         margin-right: 15px;
+        margin-bottom: -5px;
         text-decoration: none;
         font-size: 17px;
         background: none;
         border: none;
-        border-bottom: 5px solid transparent;
+        border-bottom: 8px solid transparent;
         transition: 0.3s;
     }
 
     .terkini-button:hover {
-        
+        border-bottom: 8px solid #CCCCCC;
     }
 
     .terkini-button.active {
-        border-bottom: 6px solid #DD8E3F;
+        border-bottom: 8px solid #DD8E3F;
     }
 
+    .loadmore {
+        text-align: center;
+        margin-top: 30px
+    }
+
+    .loadmore-button {
+        border: 2px solid #DD8E3F;
+        background: #DD8E3F;
+        color: white;
+        padding: 10px 50px;
+        display: inline-block;
+        font-size: 17px;
+        transition: 0.3s;
+    }
+    
 </style>
 @endpush
 @section('content')
-    {{-- slider show --}}
-    @push('home-sliders')
-        <section class="home-slider">
-            @include('home.slider')
-        </section>
-    @endpush
+{{-- slider show --}}
+@push('home-sliders')
+<section class="home-slider">
+    @include('home.slider')
+</section>
+@endpush
 
-    <section class="section-trending">
-        <h3 class="subtitle">DATA TRENDING</h3>
-        <button class="trending-button">Covid 19</button>
-        <button class="trending-button active">Negara Termiskin</button>
-        <button class="trending-button">Lorem Ipsum</button>
-        <button class="trending-button">Dolor sit amet</button>
-        <button class="trending-button">Investasi Asing</button>
-        <button class="trending-button">Lorem Ipsum</button>
-        <button class="trending-button">Dolor sit amet</button>
-        <button class="trending-button">Investasi Lokal</button>
-    </section>
+<section class="section-trending">
+    <h3 class="subtitle">DATA TRENDING</h3>
+    <button class="trending-button">Covid 19</button>
+    <button class="trending-button active">Negara Termiskin</button>
+    <button class="trending-button">Lorem Ipsum</button>
+    <button class="trending-button">Dolor sit amet</button>
+    <button class="trending-button">Investasi Asing</button>
+    <button class="trending-button">Lorem Ipsum</button>
+    <button class="trending-button">Dolor sit amet</button>
+    <button class="trending-button">Investasi Lokal</button>
+</section>
 
-    <section class="section-terkini">
-        <h3 class="subtitle">DATA TERKINI</h3>
-        <div class="terkini-wrap">
-            <button class="terkini-button active" >Semua Topik</button>
-            <button class="terkini-button">Demografi</button>
-            <button class="terkini-button">Moneter</button>
-            <button class="terkini-button">Keuangan</button>
-            <button class="terkini-button">PDB</button>
-            <button class="terkini-button">Energi</button>
-            <button class="terkini-button">Perdagangan</button>
-            <button class="terkini-button">Properti</button>
-            <button class="terkini-button">Pasar</button>
+<section class="section-terkini">
+    <h3 class="subtitle">DATA TERKINI</h3>
+    <div class="terkini-wrap">
+        <button class="terkini-button active">Semua Topik</button>
+        <button class="terkini-button">Demografi</button>
+        <button class="terkini-button">Moneter</button>
+        <button class="terkini-button">Keuangan</button>
+        <button class="terkini-button">PDB</button>
+        <button class="terkini-button">Energi</button>
+        <button class="terkini-button">Perdagangan</button>
+        <button class="terkini-button">Properti</button>
+        <button class="terkini-button">Pasar</button>
+    </div>
+    <div class="terkini list">
+        <div class='container pd-container'>
+            <div class='row pd-row1'>
+                @for ($i = 0; $i < 12; $i++)
+                    
+                    <div class='pd-area col-md-4'>
+                        <a href='#'>
+                            <div class='effect-bubba pd-rec'><img class='img-reponsive'
+                                    src='http://www.pulatech.com/wp-content/uploads/2015/11/mobile-apps.jpg' />
+                                <figcaption>
+
+                                </figcaption>
+                            </div>
+                        </a>
+                        <div class='pd-des pd-sq-des'>
+                            <h2>Inilah 10 Negara Termiskin di Dunia pada 2021</h2>
+                            <div class="row">
+                                <div class="col-9">
+                                    <p>INTERNASIONAL</p>
+                                    <p>26/04/2021 14.18 WIB</p>
+                                </div>
+                                <div class="col-3 share-logo">
+                                    <img class="img-responsive" src="{{ asset('databoks-assets/images/share.svg') }}">
+                                </div>
+                            </div>                            
+                        </div>
+                    </div>
+
+                @endfor
+
+            </div>
         </div>
-    </section>
+        <div class="loadmore">
+            <button class="loadmore-button">Muat lebih banyak</button>
+        </div>
+    </div>
+</section>
+
+<section class="section-topik">
+    <h3 class="subtitle">TOPIK PILIHAN</h3>
+</section>
 @stop
 
 @push('scripts')
-    <script>
-        $(document).on('click', '.trending-button', function(){
-            $(this).addClass('active').siblings().removeClass('active')
-        })
-        $(document).on('click', '.terkini-button', function(){
-            $(this).addClass('active').siblings().removeClass('active')
-        })
-    </script>
+<script>
+    $(document).on('click', '.trending-button', function () {
+        $(this).addClass('active').siblings().removeClass('active')
+    })
+    $(document).on('click', '.terkini-button', function () {
+        $(this).addClass('active').siblings().removeClass('active')
+    })
+</script>
 @endpush
